389d57cb4542a9af0f478ac33221370136a6ec6f
Alexis Novas <alexismoises@hotmail.com>
2021-08-11 11:58:59 -0400

    Part 3 Finished (with likes bug)

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,37 @@
+import { React, useState, useEffect} from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import { useDispatch } from 'react-redux';
+
+import useStyles from './styles';
+import { getPosts } from '../../actions/posts';
+
+const Home = () => {
+    const classes = useStyles();
+    const [currentId, setCurrentId] = useState(0);
+    const dispatch = useDispatch();
+    
+
+    useEffect(() => {
+        dispatch(getPosts());
+      }, [dispatch]);
+
+    return (
+        <Grow in>
+            <Container>
+                <Grid className = {classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                         <Posts setCurrentId = {setCurrentId}/>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId = {currentId} setCurrentId = {setCurrentId}/>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    )
+}
+
+export default Home
+
